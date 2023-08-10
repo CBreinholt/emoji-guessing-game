e805197a019cf47e8da76f000f91fe209c13074d
CBreinholt <cmbreinholt@gmail.com>
2023-08-10 17:05:38 -0600

    Separate components from styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,32 @@
+import React, { useState } from 'react';
+import EmojiDisplay from './EmojiDisplay';
+import GuessInput from './GuessInput';
+import Modal from './Modal';
 import '../styles/App.css';
 
 function App() {
+  const emojiData = [
+    { emoji: "😀", text: "grinning" },
+    { emoji: "😃", text: "smiley" },
+    { emoji: "😄", text: "happy" },
+    // ... You can add more emojis and their corresponding text values here
+  ];
+
+  const randomEmoji = emojiData[Math.floor(Math.random() * emojiData.length)];
+
+  const [selectedEmoji] = useState(randomEmoji);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
+
+  const handleSubmit = (guess) => {
+    if (guess === selectedEmoji.text) {
+      setModalMessage("Correct Guess!");
+    } else {
+      setModalMessage(`Incorrect! The correct answer was "${selectedEmoji.text}".`);
+    }
+    setIsModalOpen(true);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -8,6 +34,15 @@ function App() {
           Emoji Guessing Game
         </p>
       </header>
+      <div className="Game">
+        <EmojiDisplay emoji={selectedEmoji.emoji} />
+        <GuessInput onSubmit={handleSubmit} />
+        <Modal
+          isOpen={isModalOpen}
+          message={modalMessage}
+          onClose={() => setIsModalOpen(false)}
+        />
+      </div>
     </div>
   );
 }
