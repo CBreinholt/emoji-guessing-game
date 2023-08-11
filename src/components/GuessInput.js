2772343eb4d7aac099551a863f88d0f66b826f74
CBreinholt <cmbreinholt@gmail.com>
2023-08-10 22:07:40 -0600

    Update guess submit button styles

--- src/components/GuessInput.js
+++ src/components/GuessInput.js
@@ -1,20 +1,55 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function GuessInput({ onSubmit }) {
-  const [guess, setGuess] = useState("");
+function GuessInput({ value, onChange, onSubmit }) {
+
+  const inputStyle = {
+    padding: '10px',
+    fontSize: '1em',
+    width: '80%',
+    marginBottom: '20px',
+    borderRadius: '5px',
+    border: '1px solid #ccc'
+  };
+
+  const buttonStyle = {
+    padding: '10px 20px',
+    fontSize: '1em',
+    backgroundColor: '#007BFF',
+    color: '#fff',
+    border: 'none',
+    borderRadius: '5px',
+    transition: '0.3s',
+  };
+
+  const handleInputChange = (e) => {
+    onChange(e.target.value);
+  };
+
+  const handleButtonClick = () => {
+    onSubmit();
+  };
 
   return (
     <div>
       <input
         type="text"
-        value={guess}
-        onChange={(e) => setGuess(e.target.value)}
+        value={value}
+        onChange={handleInputChange}
         placeholder="Enter your guess"
+        style={inputStyle}
       />
-      <button onClick={() => onSubmit(guess)}>Submit</button>
+      <button 
+        onClick={handleButtonClick}
+        style={buttonStyle}
+        onMouseOver={(e) => e.target.style.backgroundColor = '#0056b3'}
+        onMouseOut={(e) => e.target.style.backgroundColor = '#007BFF'}
+      >
+        Submit
+      </button>
     </div>
   );
 }
+
 
 export default GuessInput;
 
